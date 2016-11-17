98d7fafc202463321eb010280a839cdae3de3d4c
Juvid <jutewei@qq.com>
2016-11-17 11:14:48 +0800

    Initial Commit

---
+++ JuRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JuRefresh
+//
+//  Created by Juvid on 2016/11/17.
+//  Copyright © 2016年 Juvid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
